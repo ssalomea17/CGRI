51b9262e08eb059d20aad7bdaa70e4c98330ea97
Ruben <ruben.hategan94@gmail.com>
2021-01-17 23:13:55 +0200

    updates

---
+++ IomCIS/Assets/Fuchsia/Scrits/Child/moving.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moving : MonoBehaviour
+{
+    Animation animatie;
+
+    public GameObject caracter;
+
+    float viteza = 2.0f;
+
+
+    Vector3 initial;
+    bool walk = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Renderer>().enabled = false;
+        animatie = caracter.GetComponent<Animation>();
+        initial = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("1"))
+        {
+            walk = true;
+            gameObject.GetComponent<Renderer>().enabled = true;
+        }
+        if (walk)
+        {
+            if (Vector3.Distance(caracter.transform.position, initial) < 3)
+            {
+                transform.Translate(0, 0, Time.deltaTime * viteza);
+                animatie.Play("Walking");
+            }
+            else
+            {
+                animatie.Stop("Walking");
+            }
+        }
+
+        //print(Vector3.Distance(caracter.transform.position, initial));
+    }
+}
--- IomCIS/Assets/Soja Exiles/SE Basic Assets/Scripts and Animations/Doors/opencloseDoor1.cs
+++ IomCIS/Assets/Soja Exiles/SE Basic Assets/Scripts and Animations/Doors/opencloseDoor1.cs
@@ -1,30 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class opencloseDoor1: MonoBehaviour {
+public class opencloseDoor1 : MonoBehaviour
+{
 
 	public Animator openandclose1;
 	public bool open;
 	public Transform Player;
 
-	void Start (){
+	void Start()
+	{
 		open = false;
 	}
 
-	void OnMouseOver (){
+	void OnMouseOver()
+	{
 		{
-			if (Player) {
-				float dist = Vector3.Distance (Player.position, transform.position);
-				if (dist < 15) {
-					if (open == false) {
-						if (Input.GetMouseButtonDown (0)) {
-							StartCoroutine (opening ());
+			if (Player)
+			{
+				float dist = Vector3.Distance(Player.position, transform.position);
+				if (dist < 15)
+				{
+					if (open == false)
+					{
+						if (Input.GetMouseButtonDown(0))
+						{
+							StartCoroutine(opening());
 						}
-					} else {
-						if (open == true) {
-							if (Input.GetMouseButtonDown (0)) {
-								StartCoroutine (closing ());
+					}
+					else
+					{
+						if (open == true)
+						{
+							if (Input.GetMouseButtonDown(0))
+							{
+								StartCoroutine(closing());
 							}
 						}
 
@@ -36,19 +48,43 @@ public class opencloseDoor1: MonoBehaviour {
 		}
 
 	}
+	void Update()
+	{
+		if (Vector3.Distance(Player.position, transform.position) < 2)
+		{
+			if (open == false)
+			{
+					StartCoroutine(opening());
+				
+			}
 
-	IEnumerator opening(){
-		print ("you are opening the door");
-		openandclose1.Play ("Opening 1");
-		open = true;
-		yield return new WaitForSeconds (.5f);
+		}
 	}
 
-	IEnumerator closing(){
-		print ("you are closing the door");
-		openandclose1.Play ("Closing 1");
+	private void OnTriggerEnter(Collider other)
+	{
+		print(other.gameObject.name);
+		if (other.gameObject.name == "LeftForeArm")
+		{
+			print("touched");
+			StartCoroutine(opening());
+		}
+	}
+
+	IEnumerator opening()
+	{
+		print("you are opening the door");
+		openandclose1.Play("Opening 1");
+		open = true;
+		yield return new WaitForSeconds(.5f);
+	}
+
+	IEnumerator closing()
+	{
+		print("you are closing the door");
+		openandclose1.Play("Closing 1");
 		open = false;
-		yield return new WaitForSeconds (.5f);
+		yield return new WaitForSeconds(.5f);
 	}
 
 
